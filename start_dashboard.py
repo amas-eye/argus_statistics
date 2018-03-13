a69c77741bae15d3e42955d8fc8b35de3293dfb5
EaconTang <tyingk@163.com>
2018-03-13 17:48:53 +0800

    reformat code

--- start_dashboard.py
+++ start_dashboard.py
@@ -14,13 +14,16 @@ from argus_statistics.alert_stat import main as alert_stat_update
 from argus_statistics.alert_trend import main as alert_trend_update
 from argus_statistics.metric_stat import update_metric
 from argus_statistics.sys_resource import update_sys_resource
-# from argus_statistics.overall_health import main as overall_update
 from argus_statistics.host_stat import main as host_stat_update
 
-config = os.getcwd() + "/etc/update_config.json"
-
-logging.basicConfig(filename="app_record.log", level=logging.DEBUG,
-                    format='%(asctime)s-%(levelname)s-%(module)s:%(lineno)d-%(funcName)s: %(message)s')
+base_dir = os.path.dirname(os.path.abspath(__file__))
+config = os.path.join(base_dir, "etc", "update_config.json")
+pidfile = os.path.join(base_dir, 'pidfile')
+logging.basicConfig(
+    filename=os.path.join(base_dir, 'logs', "statistics.log"),
+    level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s %(module)s:%(lineno)d: %(message)s'
+)
 
 
 def read_config(config_file):
@@ -64,7 +67,7 @@ def entrance():
 
 
 def write_pid():
-    f = open('./pidfile', 'w')
+    f = open(pidfile, 'w')
     try:
         f.write(str(os.getpid()))
         logging.warn('write pid {0}'.format(os.getpid()))
@@ -78,12 +81,12 @@ def start():
 
 
 def kill():
-    with open('./pidfile', 'r') as f:
+    with open(pidfile, 'r') as f:
         pid = f.read().strip()
     try:
         os.kill(int(pid), signal.SIGTERM)
         print('Stopping server')
-        os.remove('./pidfile')
+        os.remove(pidfile)
         time.sleep(1)
     except OSError as e:
         logging.error("No such process [{}], msg {}".format(pid, e))
@@ -99,11 +102,8 @@ def main():
     这是dashboard的主入口函数
     :return:
     """
-    # logging.basicConfig(filename="app_record.log", level=logging.DEBUG,
-    # format = '%(asctime)s-%(levelname)s-%(module)s:%(lineno)d-%(funcName)s: %(message)s')
     logging.debug("start_dashboard.py is starting")
     time_interval = read_config(config) * 60
-    print(time_interval)
     update_list = [alert_stat_update, alert_trend_update, update_metric, host_stat_update, update_sys_resource]
     while True:
         Thread_list = []
